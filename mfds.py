3d51e6c65ab49863d7f51bd6302753adf2a6d207
brotherstone97 <brotherstone97@gmail.com>
2022-04-09 15:12:41 +0900

    get_last_page()함수 추가 및 show_detail()에서 호출

--- mfds.py
+++ mfds.py
@@ -4,31 +4,41 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def show_detail(driver):
-    #의약품 테이블의 tr태그 개수를 세기 위함
-    # tbody = driver.find_element(By.XPATH, '//*[@id="con_body"]/div[2]/div[3]/div[3]/table/tbody')
-    # number_of_row = len(tbody.find_elements(By.TAG_NAME, 'tr'))
-    #
-    # #조회결과가 없을 때 함수 종료
-    # if number_of_row==1:
-    #     return
-    # print(number_of_row)
-    #
-    # #tr태그 개수 만큼 반복
-    # for i in range(1, number_of_row + 1):
-    #     name = driver.find_element(By.XPATH,
-    #                                f'//*[@id="con_body"]/div[2]/div[3]/div[3]/table/tbody/tr[{i}]/td[2]/span[2]/a')
-    #     print(name.text)
 
-    #다음페이지로 이동
-    last_page = driver.find_element(By.CLASS_NAME, 'page_last')
-    last_page_onclick_value = last_page.get_attribute('onclick')
-    if not last_page_onclick_value:
-        return
-    print(last_page_onclick_value)
-    # splited_value = last_page_onclick_value.split('(')
-    # print(splited_value[1][:-1])
+    last_page = get_last_page()
 
+    for i in range(last_page):
+        #의약품 테이블의 tr태그 개수를 세기 위함
+        tbody = driver.find_element(By.XPATH, '//*[@id="con_body"]/div[2]/div[3]/div[3]/table/tbody')
+        number_of_row = len(tbody.find_elements(By.TAG_NAME, 'tr'))
 
+        #조회결과가 없을 때 함수 종료
+        if number_of_row==1:
+            return
+        print(number_of_row)
+
+        #tr태그 개수 만큼 반복
+        for j in range(1, number_of_row + 1):
+            name = driver.find_element(By.XPATH,
+                                       f'//*[@id="con_body"]/div[2]/div[3]/div[3]/table/tbody/tr[{j}]/td[2]/span[2]/a')
+            print(name.text)
+
+ # 최대 페이지 정보를 얻기 위함.(onclick attribute의 value이용)
+def get_last_page():
+    try:
+        last_page = driver.find_element(By.CLASS_NAME, 'page_last')
+        last_page_onclick_value = last_page.get_attribute('onclick')
+        if not last_page_onclick_value:
+            return 1
+        print(last_page_onclick_value)
+
+        # value에서 정수만을 추출하기 위함
+        splited_value = last_page_onclick_value.split('(')
+        print(splited_value[1][:-1])
+        return splited_value
+    except:
+        #항목이 없을 경우
+        return 1
 
 
 def mfds_total_drug(driver):
