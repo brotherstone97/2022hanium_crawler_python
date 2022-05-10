4510475e130beef84bd187363ed852045b0a2340
brotherstone97 <brotherstone97@gmail.com>
2022-05-10 21:26:12 +0900

    Fix: complete DUR loop

--- mfds.py
+++ mfds.py
@@ -85,12 +85,12 @@ def show_detail(driver):
             DUR_thead = driver.find_element(By.XPATH, DUR_thead_xpath)
             number_of_th = len(DUR_thead.find_elements(By.TAG_NAME, 'th'))
 
-            DUR_tbody_xpath = '//*[@id="scroll_06"]/table/tbody/'
+            DUR_tbody_xpath = '//*[@id="scroll_06"]/table/tbody'
             DUR_tbody = driver.find_element(By.XPATH, DUR_tbody_xpath)
             number_of_tbody_tr = len(DUR_tbody.find_elements(By.TAG_NAME, 'tr'))
             # number_of_tbody_td = len(DUR_tbody.find_elements(By.TAG_NAME, 'td'))
 
-            print('number_of_tbody_tr' + number_of_tbody_tr)
+            print('number_of_tbody_tr: ', number_of_tbody_tr)
             # print('number_of_tbody_td' + number_of_tbody_td)
             # number_of_td = len(DUR_tbody_xpath.find_element(By.TAG_NAME, 'td'))
 
@@ -98,7 +98,7 @@ def show_detail(driver):
             ###최종 저장되는 형태는??
 
         #     단일tr = '//*[@id="scroll_06"]/table/tbody/tr'
-        #     다중tr = '// *[ @ id = "scroll_06"] / table / tbody / tr[1]'
+        #     다중tr = '//*[@id="scroll_06"]/table/tbody/tr[1]'
         #         '// *[ @ id = "scroll_06"] / table / tbody / tr[2]'
         # 다중tr의td콘텐츠 = '// *[ @ id = "scroll_06"] / table / tbody / tr[1] / td[1] / span[2]'
 
@@ -108,21 +108,33 @@ def show_detail(driver):
 
         # td는 총 6개, tr[j] / td[k] / span[2]
 
+        # tr==1: tbody/ tr / td[{j}] / span[2]
+        # tr==2: tbody/ tr[{i}]/td[{j}]/ span[2]
+
         ####tr subscript문제 해결할 것
         save_tester['DUR'] = []
-        td_contents_xpath_prefix= ''
-        if number_of_tbody_tr==1:
-        for i in range(0, number_of_tbody_tr):
-            list_element={}
-            for j in range(1, number_of_th+1):
+
+        # DUR테이블 행 하나당 dict하나로 저장
+        for i in range(1, number_of_tbody_tr + 1):
+            # DUR테이블의 콘텐츠 tr(행)의 개수에 따라 xpath를 다르게 지정.
+            if number_of_tbody_tr <= 1:
+                td_contents_xpath_prefix = f'// *[ @ id = "scroll_06"] / table / tbody / tr /'
+            else:
+                td_contents_xpath_prefix = f'// *[ @ id = "scroll_06"] / table / tbody / tr[{i}] /'
+            #
+            list_element = {}
+            for j in range(1, number_of_th + 1):
                 head_name_xpath = f'// *[ @ id = "scroll_06"] / table / thead / tr / th[{j}]'
                 head_name = driver.find_element(By.XPATH, head_name_xpath).text
-                td_contents_xpath=f'// *[ @ id = "scroll_06"] / table / tbody / tr / td[{j}] / span[2]'
-                td_contents = driver.find_element(By.XPATH, td_contents_xpath).text
-                list_element[head_name] = td_contents
-                print('list_element'+list_element)
+                try:
+                    td_contents_xpath = f'{td_contents_xpath_prefix} td[{j}] / span[2] | {td_contents_xpath_prefix} td[{j}] / a'
+                    td_contents = driver.find_element(By.XPATH, td_contents_xpath).text
+                    list_element[head_name] = td_contents
+                except:
+                    list_element[head_name] = ''
+                print('list_element: ', list_element)
             save_tester['DUR'].append(list_element)
-        print(save_tester)
+    print(save_tester)
 
 
 # 최대 페이지 정보를 얻기 위함.(onclick attribute의 value이용)
--- tester.py
+++ tester.py
@@ -3,18 +3,44 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path='chromedriver.exe')
 
-URL = 'https://nedrug.mfds.go.kr/pbp/CCBBB01/getItemDetail?itemSeq=200402915'
+URL = 'https://nedrug.mfds.go.kr/pbp/CCBBB01/getItemDetail?itemSeq=202105574'
 
 driver.get(url=URL)
 
+# DUR(의약품 적정 사용 정보)
+DUR_thead_xpath = '//*[@id="scroll_06"]/table/thead/tr'
+DUR_thead = driver.find_element(By.XPATH, DUR_thead_xpath)
+number_of_th = len(DUR_thead.find_elements(By.TAG_NAME, 'th'))
+
 DUR_tbody_xpath = '//*[@id="scroll_06"]/table/tbody'
 DUR_tbody = driver.find_element(By.XPATH, DUR_tbody_xpath)
-
-
 number_of_tbody_tr = len(DUR_tbody.find_elements(By.TAG_NAME, 'tr'))
-number_of_tbody_td = len(DUR_tbody.find_elements(By.TAG_NAME, 'tr').find_elements(By.TAG_NAME, 'td'))
-print('number_of_tbody_tr', number_of_tbody_tr)
-print('number_of_tbody_td', number_of_tbody_td)
+# number_of_tbody_td = len(DUR_tbody.find_elements(By.TAG_NAME, 'td'))
+
+print('number_of_tbody_tr: ', number_of_tbody_tr)
+
+save_tester= {'DUR': []}
 
 
+# DUR테이블 행 하나당 dict하나로 저장
+for i in range(1, number_of_tbody_tr+1):
+    # DUR테이블의 콘텐츠 tr(행)의 개수에 따라 xpath를 다르게 지정.
+    if number_of_tbody_tr <= 1:
+        td_contents_xpath_prefix = f'// *[ @ id = "scroll_06"] / table / tbody / tr /'
+    else:
+        td_contents_xpath_prefix = f'// *[ @ id = "scroll_06"] / table / tbody / tr[{i}] /'
+    #
+    list_element = {}
+    for j in range(1, number_of_th + 1):
+        head_name_xpath = f'// *[ @ id = "scroll_06"] / table / thead / tr / th[{j}]'
+        head_name = driver.find_element(By.XPATH, head_name_xpath).text
+        try:
+            td_contents_xpath = f'{td_contents_xpath_prefix} td[{j}] / span[2] | {td_contents_xpath_prefix} td[{j}] / a'
+            td_contents = driver.find_element(By.XPATH, td_contents_xpath).text
+            list_element[head_name] = td_contents
+        except:
+            list_element[head_name] = ''
+        print('list_element: ', list_element)
+    save_tester['DUR'].append(list_element)
+    print(save_tester)
 driver.close()
